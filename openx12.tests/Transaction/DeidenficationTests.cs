bab011b6f1969bfee18f1fff1e05e9e6c5c1de2d
Daniel Pries <DanielMPries@gmail.com>
2019-03-17 15:58:54 -0400

    Added test to demo deidentifying a name

---
+++ openx12.tests/Transaction/DeidenficationTests.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using openx12.Models.Transaction;
+using openx12.Models;
+using XTransaction = openx12.Models.Transaction.Transaction;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+using System;
+
+namespace openx12.tests.Transaction
+{
+    [TestClass]
+    public class DeidenficationTests
+    {
+        private XTransaction _Transaction;
+
+        [TestInitialize]
+        public void Setup() {
+            var path = System.IO.Path.Combine(".","TestData","Sample834.txt");
+            var doc = X12Document.Read(path);
+            _Transaction = doc
+                    .Interchanges.First()
+                    .FunctionalGroups.First()
+                    .TransactionSets.First();
+        }
+
+        
+        [TestMethod, TestCategory("Unit")]
+        public void should_parse_transaction() {    
+            var sub = _Transaction.Segments
+                .SingleOrDefault( x => x.Name == "NM1" && x.Qualifier == "IL");
+            sub.Elements[2].Value.Should().Be("JOHN DOE");
+        }
+
+        [TestMethod, TestCategory("Unit")]
+        public void should_deidentify_name() {    
+            var sub = _Transaction.Segments
+                .SingleOrDefault( x => x.Name == "NM1" && x.Qualifier == "IL");
+            sub.Elements[2].Value = "XXXX XXX";
+            System.IO.File.WriteAllText("output.txt", _Transaction.ToString());
+        }
+    }
+}
